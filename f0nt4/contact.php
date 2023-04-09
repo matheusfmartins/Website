9ac1ee9a0e5c3338d8394923d8adea08971132d1
Matheus Fontanetti Martins <matheusfontanettimartins@gmail.com>
2023-04-09 19:10:01 -0300

    final

--- f0nt4/contact.php
+++ f0nt4/contact.php
@@ -7,9 +7,43 @@ try{
     $emailFrom = $_POST['email'];
     $message = $_POST['message'];
 
+
+    // valida as pastas no servidor
+    $date = date('d/m/Y');
+
+    // validate year directory
+    $year = explode("/",$date)[2];
+
+    if (is_dir("/contato/" . $year)){
+
+    } else{
+        mkdir("/contato/" . $year);
+    }
+
+
+    // validate month directory
+    $month = explode("/",$date)[1];
+
+    if (is_dir("/contato/" . $year . "/" . $month)){
+
+    } else{
+        mkdir("/contato/" . $year . "/" . $month);
+    }
+
+
+    // validate day directory
+    $day = explode("/",$date)[0];
+
+    if (is_dir("/contato/" . $year . "/" . $month . "/" . $day)){
+
+    } else{
+        mkdir("/contato/" . $year . "/" . $month . "/" . $day);
+    }
+
+
     // cria arquivo no servidor
-    $arquivo = fopen('/contato/nome_'. $name .'.txt','w');
-    $texto = "Nome: ". $name ."\r\nEmail: ". $emailFrom ."\r\nMensagem: ". $message;
+    $arquivo = fopen("/contato/" . $year . "/" . $month . "/" . $day . "/nome_". $name .".txt","w");
+    $texto = "Nome: ". $name ."\r\nEmail: ". $emailFrom ."\r\nMensagem: ". $message ."\r\n";
     fwrite($arquivo, $texto);
     fclose($arquivo);
 
