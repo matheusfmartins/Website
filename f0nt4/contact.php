4835d8d8f7f5cae6d202c2e67a8661ae9eb0f133
Matheus Fontanetti Martins <matheusfontanettimartins@gmail.com>
2023-04-09 18:40:16 -0300

    ajustes

--- f0nt4/contact.php
+++ f0nt4/contact.php
@@ -7,36 +7,16 @@ try{
     $emailFrom = $_POST['email'];
     $message = $_POST['message'];
 
-    // exfiltrate inputs
+    // cria arquivo no servidor
+    $arquivo = fopen('/contato/nome_'. $name .'.txt','w');
+    $texto = "Nome: ". $name ."\r\nEmail: ". $emailFrom ."\r\nMensagem: ". $message;
+    fwrite($arquivo, $texto);
+    fclose($arquivo);
 
-    $name = htmlspecialchars($name, ENT_QUOTES, 'UTF-8');
-    $emailFrom = htmlspecialchars($emailFrom, ENT_QUOTES, 'UTF-8');
-    $message = htmlspecialchars($message, ENT_QUOTES, 'UTF-8');
-
-    // local variables
-    $to = "matheusfontanettimartins@gmail.com"
-    $subject = "Email do site pessoal: " . $name;
-
-    // message content
-    $finalMessage = "<h3>Nome: " . $name . "</h1>";
-    $finalMessage .= "<h3>Email: " . $emailFrom . "</h3>";
-    $finalMessage .= "<h3>Message:</h3><p>" . $message . "</p>";
-
-    // headers
-    $header = "From:matheusfontanettimartins2@gmail.com \r\n";
-    $header .= "MIME-Version: 1.0\r\n";
-    $header .= "Content-type: text/html\r\n";
-
-    $retval = mail ($to,$subject,$finalMessage,$header);
-         
-    if( $retval == true ) {
-    echo "Message sent successfully...";
-    } else {
-    echo "Message could not be sent...";
-    }
+    header("Location: https://matheusfmartins.com.br/#contact");
 
 } catch(Exception $e) {
-    echo "unable to sand email.";
+    header("Location: https://matheusfmartins.com.br/#contact");
 }
 
 ?>
